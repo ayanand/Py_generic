efc56ca69e9d8e8558eda6c5c9a1ba3e36f4a371
ayanand <69283619+ayanand@users.noreply.github.com>
2022-05-06 16:03:56 -0400

    Update pyodbc.py

--- pyodbc.py
+++ pyodbc.py
@@ -2,10 +2,10 @@ import pyodbc
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
-server = 'regiccsmi.76755b4f90dd.database.windows.net' 
-database = 'stg' 
-username = 'regbcp' 
-password = 'regbcp' 
+server = '' 
+database = '' 
+username = '' 
+password = '' 
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
